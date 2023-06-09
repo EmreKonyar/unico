8083b4c9ffef07398492363954e5fc5ba856d21e
mehmet.ali <ozdogan.7547@gmail.com>
2023-06-09 22:19:44 +0300

    someone fixed

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from "react-native";
 import Navigation from "./src/components/Navigation";
 import { AuthProvider } from "./src/context/AuthContext";
 
--- src/components/BottomTabNavigator.js
+++ src/components/BottomTabNavigator.js
@@ -2,18 +2,15 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
 import HomeScreen from '../screens/HomeScreen';
 import ChatScreen from '../screens/ChatScreen';
-import { Text } from 'react-native';
 const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => {
   return (
-    /*
+
     <Tab.Navigator screenOptions={{headerShown: false}}>
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Chat" component={ChatScreen} />
     </Tab.Navigator>
-    */
-   <Text>Bok BU</Text>
   );
 }
 
